d16b0b71ba4cd6d94c2a23d4dda9f1b4f534f7af
Nicholas Alcazar <nicholasfalcazar@gmail.com>
2025-07-18 01:31:11 -0700

    DB interf, work in progress

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# TODO: How to pass protect / safe way to store DB info
+# TODO: Add interface for Keyring
 
 source ./util/add_root_env.sh
+source ~/Keyring/add_keyring_env.sh
+TIMESCALE_SERVICE_URL=postgres://postgres:password@localhost:5432/postgres
+
 source $PROJ_ROOT/app/venv/bin/activate
 python $PROJ_ROOT/app/pol_app.py
